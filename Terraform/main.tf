23ac5f5c545744e2f7fcd7ebfcbbfad40bbcb889
DavideLongo44 <davidelongo44@gmail.com>
2024-04-24 13:38:11 +0200

    terraform update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,26 +20,6 @@ resource "aws_subnet" "control-panel" {
   }
 }
 
-resource "aws_subnet" "ansible" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "eu-central-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Ansible Subnet"
-  }
-}
-
-resource "aws_subnet" "ansible2" {
-  vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.4.0/24"
-  availability_zone       = "eu-central-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "Ansible Subnet2"
-  }
-}
-
 resource "aws_internet_gateway" "example_igw" {
   vpc_id = aws_vpc.example_vpc.id
 }
@@ -82,6 +62,19 @@ resource "aws_security_group" "example_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Grafana access for 3000
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress  {
     from_port   = 0
     to_port     = 0
@@ -101,19 +94,18 @@ resource "aws_instance" "control_panel" {
   user_data = <<-EOF
     #!/bin/bash
     yum update -y
-    yum install -y ansible git
+    yum install -y ansible git 
     systemctl enable ansible
     systemctl start ansible
-    git clone -b DevDav https://github.com/DavideLongo44/shop.git /tmp/shop
-    cp -r /tmp/shop/Terraform/ansible /ansible
-    rm -rf /tmp/shop
+    git clone --depth=1 --branch=Ansible https://github.com/DavideLongo44/shop.git home/ec2-user/ansible 
   EOF
 }
+
 resource "aws_instance" "ansible" {
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   ami                    = "ami-0f673487d7e5f89ca"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.ansible.id
+  subnet_id              = aws_subnet.control-panel.id
   tags = {
     Name = "ansible"
   }
@@ -131,8 +123,17 @@ resource "aws_instance" "ansible2" {
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   ami                    = "ami-0f673487d7e5f89ca"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.ansible2.id
+  subnet_id              = aws_subnet.control-panel.id
   tags = {
-    Name = "ansible2"
+    Name = "monitor"
   }
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y docker
+    systemctl enable docker
+    systemctl start docker
+    docker run -d -p 3000:3000 --name=grafana grafana/grafana:latest
+    docker run -d -p 9090:9090 --name=prometheus prom/prometheus:latest
+  EOF
 }
