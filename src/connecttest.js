3dab1812fa70b5c5039bc9c211d694dc497d027f
Fabian schmelzing <schmelzing1995@gmail.com>
2024-04-26 01:15:08 +0200

    update

--- src/connecttest.js
+++ src/connecttest.js
@@ -1,13 +1,15 @@
+
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017/';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = 'mongodb://localhost:27017/shopwise';
+const client = new MongoClient(uri);
 
 async function main() {
   try {
     await client.connect();
     console.log('Connected to MongoDB');
     // Führe hier Datenbankoperationen durch...
+
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   } finally {
